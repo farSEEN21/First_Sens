c5d03606cdf1fe29a50b591fb39ed1fe2a2f2c5a
farSEEN21 <58916437+farSEEN21@users.noreply.github.com>
2023-06-12 09:37:37 +0300

    XPath

--- src/test/java/StartSel.java
+++ src/test/java/StartSel.java
@@ -19,24 +19,29 @@ wd.get("https://telranedu.web.app/home");
 //old stratagies
 wd.findElement(By.tagName("a"));
 wd.findElement(By.cssSelector("a"));
-
+wd.findElement(By.xpath("//a"));
 
 wd.findElement(By.id("root"));
 wd.findElement(By.cssSelector("#root"));
+wd.findElement(By.xpath("//div[@id='root']"));
 
 wd.findElement(By.className("container"));
 wd.findElement(By.cssSelector(".container"));
+wd.findElement(By.xpath("//body/div[@class='container']"));
 
 wd.findElement(By.linkText("HOME"));
 wd.findElement(By.partialLinkText("HO"));
+wd.findElement(By.xpath("//a[@href='/home']"));
 
 wd.findElement(By.cssSelector("[href='/login']"));
 wd.findElement(By.cssSelector("[href*='og']"));
 wd.findElement(By.cssSelector("[href^='/log']"));
 wd.findElement(By.cssSelector("[href$='in']"));
+wd.findElement(By.xpath("//div/a[@href='/login']"));
 
 wd.findElement(By.cssSelector("[href='/about'"));
 wd.findElement(By.cssSelector(".active"));
+wd.findElement(By.xpath("//*[@href='/about']"));
 
     }
 
