93d09943ccadd910a23b140e7e8eef74fc1fe340
farSEEN21 <58916437+farSEEN21@users.noreply.github.com>
2023-06-13 18:43:50 +0300

    hw

--- src/test/java/StartSel.java
+++ src/test/java/StartSel.java
@@ -24,6 +24,7 @@ wd.findElement(By.xpath("//a"));
 wd.findElement(By.id("root"));
 wd.findElement(By.cssSelector("#root"));
 wd.findElement(By.xpath("//div[@id='root']"));
+wd.findElement(By.xpath("//*[@id=\"root\"]"));
 
 wd.findElement(By.className("container"));
 wd.findElement(By.cssSelector(".container"));
@@ -32,12 +33,16 @@ wd.findElement(By.xpath("//body/div[@class='container']"));
 wd.findElement(By.linkText("HOME"));
 wd.findElement(By.partialLinkText("HO"));
 wd.findElement(By.xpath("//a[@href='/home']"));
+wd.findElement(By.xpath("//a[text()='HOME']"));
+wd.findElement(By.xpath("//a[.='HOME']"));
 
 wd.findElement(By.cssSelector("[href='/login']"));
 wd.findElement(By.cssSelector("[href*='og']"));
+wd.findElement(By.xpath("//div/a[contains(@href,'og')]"));
 wd.findElement(By.cssSelector("[href^='/log']"));
+wd.findElement(By.xpath("//div/a[starts-with(@href,'/log')]"));
 wd.findElement(By.cssSelector("[href$='in']"));
-wd.findElement(By.xpath("//div/a[@href='/login']"));
+
 
 wd.findElement(By.cssSelector("[href='/about'"));
 wd.findElement(By.cssSelector(".active"));
